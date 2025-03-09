b1048ffe665a5b334f3071a2d59159fa63e6fd34
fasil916 <71936145+fasil916@users.noreply.github.com>
2025-03-09 12:42:05 +0530

    Create bash-trivy-docker-image-scan.sh

---
+++ bash-trivy-docker-image-scan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DockerImage=$(awk '/^FROM/ {print $2; exit}' Dockerfile)
+
+echo "Docker Image: $DockerImage"
+
+docker run --rm -v .:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 0 --severity HIGH --light $DockerImage
+
+docker run --rm -v .:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 1 --severity CRITICAL --light $DockerImage
+
+exit_code=$?
+
+echo "Exit code: $exit_code"
+
+if [[ "$exit_code" -eq 1 ]]; then
+    echo "Image scanning found vulnerabilities!"
+else
+    echo "Image scan passed."
+fi
